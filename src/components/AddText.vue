4b6eafe25b94e0cb596da40b4980025e3f3023ea
dusenaberawilliam <46653327+dusenaberawilliam@users.noreply.github.com>
2022-10-28 16:20:42 +0200

    initial commit

---
+++ src/components/AddText.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="text" v-model="inputText" @change="addNewText" />
+    <text-list :textList="filterText"></text-list>
+  </div>
+</template>
+
+<script>
+import TextList from "./TextList.vue";
+import { ref, computed } from "vue";
+export default {
+  components: { TextList },
+  setup() {
+    const inputText = ref("");
+    const arrText = ref([]);
+
+    const filterText = computed(function () {
+      return arrText.value.filter((txt) => txt != "React");
+    });
+
+    function addNewText() {
+      arrText.value.push(inputText.value);
+      inputText.value = "";
+    }
+
+    return {
+      inputText: inputText,
+      filterText: filterText,
+      addNewText: addNewText,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TextList.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>{{ greetings }}</p>
+  <p>___________________</p>
+  <p v-for="(txt, index) in textList" :key="index">
+    {{ txt }}
+  </p>
+</template>
+
+<script>
+import { ref, watch, toRefs } from "vue";
+export default {
+  props: {
+    textList: Array,
+  },
+  setup(props) {
+    const greetings = ref("Hello! Type and hit enter");
+    const { textList } = toRefs(props);
+
+    watch(textList, (newValue, oldValue) => {
+      console.log("old", oldValue);
+      console.log("New", newValue);
+    });
+
+    return {
+      greetings: greetings,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
